f4f4b52167f4b9b8a7a724dc22ca3ca12d88c578
laonrd0 <laonrdo@gmail.com>
2022-06-06 14:15:58 -0500

    add catalogos migrations

---
+++ database/migrations/2022_06_06_191133_create_catalogos_items.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('catalogos_items', function (Blueprint $table) {
+            $table->id();
+            $table->integer('cataogo_id');
+            $table->string('clave', 8);
+            $table->string('name');
+            $table->boolean('active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('catalogos_items');
+    }
+};
